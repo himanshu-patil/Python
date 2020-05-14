6e406a0d631183bddfb0457b4710734248bbbb01
himanshu-patil <himanshu15122000@gmail.com>
2020-05-14 17:57:40 +0530

    first commit

---
+++ addconstructor.py
@@ -0,0 +1,13 @@
+class Added:
+
+    def __init__(self, r, d):
+        self.a=r
+        self.b=d
+
+    def addition(self):
+        s=0
+        self.s = self.a+self.b
+        print(self.s)
+
+s1=Added(3,4)
+s1.addition()
